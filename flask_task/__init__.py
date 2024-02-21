b2cf367cce525c51683e017b16e7fed1d17a9fec
rohit9917 <rohit.rawat@tothenew.com>
2024-02-21 17:57:44 +0530

    thriteen commit

--- flask_task/__init__.py
+++ flask_task/__init__.py
@@ -1,9 +1,10 @@
+from flask import Flask
 from flask_task import Flask
 
 # login_view = 'users.login'
 
 def create_app():
-
+    
     app = Flask(__name__)
     
     from flask_task import users

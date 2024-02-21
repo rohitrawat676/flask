8e3df8e6de1d29e9124b3da8ee242cf3610841a6
rohit9917 <rohit.rawat@tothenew.com>
2024-02-21 22:49:37 +0530

    tweenty_five commit

--- flask_task/__init__.py
+++ flask_task/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_task.users.routes import users
 
+
 def create_app():
     
     app = Flask(__name__)
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+   
+    app.app_context().push()
     
     from flask_task import users
     

5040b73b8b819d54957a47ba8afbdb85d29f680f
rohit9917 <rohit.rawat@tothenew.com>
2024-02-21 17:43:07 +0530

    eigth commit

--- flask_task/users/routes.py
+++ flask_task/users/routes.py
@@ -1,20 +1,14 @@
 from flask import render_template, url_for, flash, redirect, request, Blueprint
 
-
 users = Blueprint('users', __name__)
 
+@users.route("/login")
+def display():
+    
+    data = {
+        'Hospital': "Flex",
+        'Specialist' : "Cancer",
+        'type': "Goverment"  
+    }
 
-@users.route("/login", methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('main.home'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('main.home'))
-        else:
-            flash('Login Unsuccessful. Please check email and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+    return render_template('display.html', title='Display', data=data)

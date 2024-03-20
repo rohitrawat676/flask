c7f3f96d240dde6268b085afeb736ba031652a4f
rohit9917 <rohit.rawat@tothenew.com>
2024-03-20 15:20:43 +0530

    MongoDB

--- mongotest.py
+++ mongotest.py
@@ -7,15 +7,15 @@ db = client["mydatabase"]
 collection = db["rohit"]
 
 data = [
-    {"name": "John Doe", "email": "john.doe@example.com", "age": 30},
-    {"name": "John Doe", "email": "john.doe@example.com", "age": 39},
-    {"name": "John Doe", "email": "john.doe@example.com", "age": 40}
+    {"name": "Rohit Rawat", "email": "john.doe@example.com", "age": 910},
+    {"name": "Nidhi Semwal", "email": "nidhi.semwal@example.com", "age": 33},
+    {"name": "Soumya Bisht", "email": "soumya.bisht@example.com", "age": 221}
 ]
 
-filter_criteria = {"age": {"$lt": 37}}
-update_operation = {"$set": {"name": "rohit"}}
+filter_criteria = {"age": {"$lt": 220}}
+update_operation = {"$set": {"name": "rohan"}}
 
-result = collection.update_many(filter_criteria, update_operation)
+result = collection.update_one(filter_criteria, update_operation)
 
 print("Matched:", result.matched_count)
 print("Modified:", result.modified_count)

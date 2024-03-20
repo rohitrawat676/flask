b23fb3b32e7a2a850e4a2aa47d2bc0d1bc0a0c55
rohit9917 <rohit.rawat@tothenew.com>
2024-03-20 12:52:22 +0530

    MongoDB

--- mongotest.py
+++ mongotest.py
@@ -4,14 +4,14 @@ client = pymongo.MongoClient("mongodb://localhost:27017/")
 
 db = client["mydatabase"]
 
-collection = db["mycollection"]
+collection = db["rohit"]
 
-data = {
-    "name": "John Doe",
-    "email": "john.doe@example.com",
-    "age": 30
-}
+data = [
+    {"name": "John Doe", "email": "john.doe@example.com", "age": 30},
+    {"name": "John Doe", "email": "john.doe@example.com", "age": 30},
+    {"name": "John Doe", "email": "john.doe@example.com", "age": 30}
+]
 
-result = collection.insert_one(data)
+result = collection.insert_many(data)
 
 print("Inserted ID:", result.inserted_id)
